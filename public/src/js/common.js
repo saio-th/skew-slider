b19602f2fe6be6e86c57e191c7ed789eb9e740bd
ysaio <ysaio@all.jp>
2019-09-27 15:38:21 +0900

    '参考サイトのslickのコードを追加（想像通り）やった'

--- public/src/js/common.js
+++ public/src/js/common.js
@@ -4,6 +4,31 @@ function slickFunc () {
   });
 }
 
+//参考サイトのスライダー（https://www.ubsc.co.jp/）
+// $(function(){
+//   $('.js-mainvisual-front').slick({
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     dots: false,
+//     asNavFor: '.js-mainvisual-slider'
+//   });
+//   $('.js-mainvisual-slider').slick({
+//     autoplay: true,
+//     autoplaySpeed: 5000,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     dots: false,
+//     focusOnSelect: true,
+//     pauseOnHover: false,
+//     asNavFor: '.js-mainvisual-front'
+//   });
+// });
+
+
 
 // --- ページの全データを読み込み後 ----------------------------------
 $(window).on('load', function() {
